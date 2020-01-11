6eb6866fe4304d97cfd21b91f2691012802eac2a
Martijn Gribnau <garm@ilumeo.com>
2020-01-11 12:37:49 +0100

    Enable a user to set its own logging output level.

--- src/bin/cargo-msrv.rs
+++ src/bin/cargo-msrv.rs
@@ -8,8 +8,8 @@ fn main() {
     init_logger();
 
     // ensure a user will see output produced by the logger
-    if !cfg!(debug_assertions) {
-        std::env::set_var("RUST_LOG", "info")
+    if !cfg!(debug_assertions) && option_env!("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "info");
     }
 
     if let Err(err) = run_cargo_msrv() {
