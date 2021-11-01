3e91aa68f506f18a5998cdad632dbfe3169b68be
Martijn Gribnau <garm@ilumeo.com>
2021-11-01 22:23:43 +0100

    Add json output for subcommand list direct-deps

--- src/dependencies/formatter/direct_deps.rs
+++ src/dependencies/formatter/direct_deps.rs
@@ -2,38 +2,31 @@ use crate::dependencies::formatter::{
     format_version, get_package_metadata_msrv, parse_manifest_workaround,
 };
 use crate::dependencies::DependencyGraph;
+use crate::reporter::Output;
+use std::marker::PhantomData;
 
-pub(crate) struct DirectDependenciesFormatter {
+pub(crate) struct DirectDependenciesFormatter<T: Output> {
     graph: DependencyGraph,
+    output: PhantomData<T>,
 }
 
-impl DirectDependenciesFormatter {
+impl<T: Output> DirectDependenciesFormatter<T> {
     pub fn new(graph: DependencyGraph) -> Self {
-        Self { graph }
+        Self {
+            graph,
+            output: PhantomData,
+        }
     }
 }
 
-impl std::fmt::Display for DirectDependenciesFormatter {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // Table of dependencies
-        use comfy_table::presets::UTF8_FULL;
-        use comfy_table::*;
+impl<T: Output> DirectDependenciesFormatter<T> {
+    fn direct_dependencies_msrv<Fi, Fg, B>(&self, init: Fi, f: Fg) -> B
+    where
+        Fi: FnOnce() -> B,
+        Fg: Fn(&mut B, Values<'_, '_>),
+    {
+        let mut out = init();
 
-        let mut table = Table::new();
-        table
-            .load_preset(UTF8_FULL)
-            .set_table_width(120) // fallback for ContentArrangement::Dynamic
-            .set_content_arrangement(ContentArrangement::Dynamic)
-            .set_header(vec!["Dependency", "Version", "MSRV", "Depends on"]);
-
-        // Table
-        // Direct dependencies
-        // e.g.
-        //
-        //
-        //
-        // | json | {version} | {MSRV} | { transitive deps with color = red if lowest version,
-        //                                  and color = orange if lower version than own MSRV }
         let dependency_graph = &self.graph;
 
         let root = &dependency_graph.root_crate;
@@ -60,16 +53,84 @@ impl std::fmt::Display for DirectDependenciesFormatter {
                 .or_else(|| get_package_metadata_msrv(package))
                 .or_else(|| parse_manifest_workaround(package.manifest_path.as_path())); // todo: add last one as option to config
 
-            table.add_row(vec![
-                Cell::new(&package.name),
-                Cell::new(&package.version),
-                Cell::new(format_version(msrv.as_ref())),
-                Cell::new(format_dependencies(&package.dependencies)),
-            ]);
+            let values = Values {
+                name: &package.name,
+                version: &package.version,
+                msrv: format_version(msrv.as_ref()),
+                dependencies: format_dependencies(&package.dependencies),
+            };
+
+            f(&mut out, values);
         }
+
+        out
+    }
+}
+
+impl std::fmt::Display for DirectDependenciesFormatter<crate::reporter::ui::HumanPrinter<'_, '_>> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Table of dependencies
+        use comfy_table::presets::UTF8_FULL;
+        use comfy_table::*;
+
+        let table = self.direct_dependencies_msrv(
+            || {
+                let mut table = Table::new();
+                table
+                    .load_preset(UTF8_FULL)
+                    .set_table_width(120) // fallback for ContentArrangement::Dynamic
+                    .set_content_arrangement(ContentArrangement::Dynamic)
+                    .set_header(vec!["Dependency", "Version", "MSRV", "Depends on"]);
+
+                table
+            },
+            |acc, next| {
+                acc.add_row(vec![
+                    Cell::new(&next.name),
+                    Cell::new(&next.version),
+                    Cell::new(&next.msrv),
+                    Cell::new(&next.dependencies),
+                ]);
+            },
+        );
 
         table.fmt(f)
     }
+}
+
+impl std::fmt::Display for DirectDependenciesFormatter<crate::reporter::json::JsonPrinter<'_, '_>> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Table of dependencies
+        use json::object;
+
+        let objects = self.direct_dependencies_msrv(
+            || Vec::new(),
+            |acc, next| {
+                acc.push(object! {
+                    dependency: next.name,
+                    version: format!("{}", next.version),
+                    msrv: next.msrv,
+                    depends_on: next.dependencies,
+                });
+            },
+        );
+
+        let json = object! {
+            reason: "list",
+            variant: crate::config::list::DIRECT_DEPS,
+            success: true,
+            list: objects,
+        };
+
+        writeln!(f, "{}", json)
+    }
+}
+
+struct Values<'s, 'v> {
+    name: &'s str,
+    version: &'v crate::semver::Version,
+    msrv: String,
+    dependencies: String,
 }
 
 fn format_dependencies(dependencies: &[cargo_metadata::Dependency]) -> String {
--- src/subcommands/list.rs
+++ src/subcommands/list.rs
@@ -1,5 +1,5 @@
 use crate::config::list::ListVariant;
-use crate::config::Config;
+use crate::config::{Config, OutputFormat};
 use crate::dependencies::resolver::{CargoMetadataResolver, DependencyResolver};
 use crate::errors::TResult;
 use crate::reporter::Output;
@@ -12,10 +12,19 @@ pub fn run_list_msrv<R: Output>(config: &Config, output: &R) -> TResult<()> {
     let graph = resolver.resolve()?;
 
     match config.sub_command_config().as_list_cmd_config().variant {
-        ListVariant::DirectDeps => {
-            let formatter = formatter::DirectDependenciesFormatter::new(graph);
-            output.write_line(&format!("{}", formatter));
-        }
+        ListVariant::DirectDeps => match config.output_format() {
+            OutputFormat::Human => {
+                use crate::reporter::ui::HumanPrinter;
+                let formatter = formatter::DirectDependenciesFormatter::<HumanPrinter>::new(graph);
+                output.write_line(&format!("{}", formatter));
+            }
+            OutputFormat::Json => {
+                use crate::reporter::json::JsonPrinter;
+                let formatter = formatter::DirectDependenciesFormatter::<JsonPrinter>::new(graph);
+                output.write_line(&format!("{}", formatter));
+            }
+            OutputFormat::None | OutputFormat::TestSuccesses => {}
+        },
         ListVariant::OrderedByMSRV => {
             let formatter = formatter::ByMSRVFormatter::new(graph);
             output.write_line(&format!("{}", formatter));
