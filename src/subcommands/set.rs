4a16df8ba320de5235a70d02bf1585fbb4eeca28
Martijn Gribnau <garm@ilumeo.com>
2022-02-15 02:35:41 +0100

    Remove MSRV prior to setting new MSRV

--- src/subcommands/set.rs
+++ src/subcommands/set.rs
@@ -4,7 +4,7 @@ use crate::paths::crate_root_folder;
 use crate::{CargoMSRVError, Config, ModeIntent, Output, TResult};
 use rust_releases::semver;
 use std::io::Write;
-use toml_edit::{value, Document};
+use toml_edit::{value, Document, Item};
 
 const RUST_VERSION_SUPPORTED_SINCE: semver::Version = semver::Version::new(1, 56, 0);
 
@@ -23,6 +23,8 @@ pub fn run_set_msrv<R: Output>(config: &Config, output: &R) -> TResult<()> {
 
     let msrv = &config.sub_command_config().set().msrv;
 
+    remove_minimum_rust_version(&mut manifest);
+
     if msrv.to_semver_version() >= RUST_VERSION_SUPPORTED_SINCE {
         manifest["package"]["rust-version"] = value(msrv.to_string());
     } else {
@@ -38,7 +40,7 @@ pub fn run_set_msrv<R: Output>(config: &Config, output: &R) -> TResult<()> {
             source: IoErrorSource::OpenFile(cargo_toml.clone()),
         })?;
 
-    writeln!(&mut file, "{}", manifest).map_err(|error| CargoMSRVError::Io {
+    write!(&mut file, "{}", manifest).map_err(|error| CargoMSRVError::Io {
         error,
         source: IoErrorSource::WriteFile(cargo_toml.clone()),
     })?;
@@ -46,4 +48,36 @@ pub fn run_set_msrv<R: Output>(config: &Config, output: &R) -> TResult<()> {
     output.finish_success(ModeIntent::Set, None);
 
     Ok(())
+}
+
+/// Removes the minimum supported Rust version (MSRV) from `Cargo.toml` manifest, if it exists
+fn remove_minimum_rust_version(document: &mut Document) {
+    /// Removes the `MSRV` as supported by Cargo since Rust 1.56.0
+    ///
+    /// [`Cargo`]: https://doc.rust-lang.org/cargo/reference/manifest.html#the-rust-version-field
+    fn remove_rust_version(document: &mut Document) {
+        document
+            .as_table_mut()
+            .get_mut("package")
+            .and_then(Item::as_table_mut)
+            .and_then(|package| package.remove("rust-version"));
+    }
+
+    /// Removes the MSRV as supported by `cargo-msrv`, since prior to the release of Rust
+    /// 1.56.0
+    fn remove_metadata_msrv(document: &mut Document) {
+        document
+            .as_table_mut()
+            .get_mut("package")
+            .and_then(Item::as_table_mut)
+            .and_then(|package| package.get_mut("metadata"))
+            .and_then(Item::as_table_mut)
+            .and_then(|metadata| metadata.remove("msrv")); // TODO: does this work?
+    }
+
+    // First remove the rust-version
+    remove_rust_version(document);
+
+    // Then remove the metadata.msrv, if it exists
+    remove_metadata_msrv(document);
 }
