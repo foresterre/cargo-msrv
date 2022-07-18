9384b097ab12168986ad138a2df0db45f77909ab
Martijn Gribnau <garm@ilumeo.com>
2022-07-19 01:17:13 +0200

    Replace term_size with terminal_size
    The former is unmaintained, and has a dependency on winapi 0.2, which contains code which a future rustc version will reject.

--- src/formatting.rs
+++ src/formatting.rs
@@ -2,13 +2,15 @@ use tabled::object::Segment;
 use tabled::{Alignment, MaxWidth, Modify, Table, Tabled};
 
 pub fn table<T: Tabled>(iter: impl IntoIterator<Item = T>) -> Table {
-    let max_width = term_size::dimensions().map(|(w, _)| w).unwrap_or(60);
+    let max_width = terminal_size::terminal_size()
+        .map(|(w, _)| w.0)
+        .unwrap_or(60);
 
     Table::new(iter).with(
         Modify::new(Segment::all())
             // A compromise between 'works usually' and still dynamically sizing based on the term width.
             // Tabled and dynamic sizing are very funky together...
-            .with(MaxWidth::wrapping(max_width / 2).keep_words())
+            .with(MaxWidth::wrapping(usize::from(max_width) / 2).keep_words())
             .with(Alignment::left())
             .with(Alignment::top()),
     )
