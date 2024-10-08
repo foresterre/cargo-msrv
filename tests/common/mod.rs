deffe31f1cecc85d5cbef6f9db30bea0f683d923
Martijn <garm@ilumeo.com>
2024-10-08 22:09:21 +0200

    chore: Port integration tests to use temp dirs

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,14 +1,40 @@
 #![allow(dead_code)]
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 pub mod reporter;
 pub mod runner;
 pub mod sub_cmd_find;
 pub mod sub_cmd_verify;
 
-pub fn fixtures_path() -> PathBuf {
-    std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("tests")
-        .join("fixtures")
+pub struct Fixture {
+    fixture_dir: PathBuf,
+    temp_dir: assert_fs::TempDir,
+}
+
+impl Fixture {
+    pub fn new(fixture_dir: impl AsRef<Path>) -> Self {
+        use assert_fs::fixture::PathCopy;
+
+        let fixture_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("tests")
+            .join("fixtures")
+            .join(fixture_dir);
+
+        let temp_dir = assert_fs::TempDir::new().unwrap();
+        temp_dir.copy_from(&fixture_dir, &["**/*"]).unwrap();
+
+        Self {
+            fixture_dir,
+            temp_dir,
+        }
+    }
+
+    pub fn tmp_path(&self, path: impl AsRef<Path>) -> PathBuf {
+        self.temp_dir.join(path)
+    }
+
+    pub fn to_str(&self) -> &str {
+        self.temp_dir.to_str().unwrap()
+    }
 }
--- tests/user_output.rs
+++ tests/user_output.rs
@@ -1,14 +1,14 @@
 use std::path::Path;
 use std::process::{Command, Stdio};
 
-use crate::common::fixtures_path;
+use crate::common::Fixture;
 
 mod common;
 
 #[test]
 fn expect_no_user_output() {
     let cargo_msrv_manifest = Path::new(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
-    let test_subject = fixtures_path().join("1.36.0");
+    let test_subject = Fixture::new("1.36.0");
 
     let process = Command::new("cargo")
         .args([
@@ -18,7 +18,7 @@ fn expect_no_user_output() {
             cargo_msrv_manifest.to_str().unwrap(),
             "--",
             "--path",
-            test_subject.to_str().unwrap(),
+            test_subject.to_str(),
             "--no-user-output", // this is the command we're testing
             "verify",
         ])
